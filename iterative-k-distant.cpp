7e4a150f13e15613232f2994e81078a07159694a
msdeep14 <mandeepsinghshekhawat95@gmail.com>
2017-07-25 19:06:37 +0530

    add code for printing k distant node of tree

---
+++ iterative-k-distant.cpp
@@ -0,0 +1,100 @@
+// CPP program to print
+// level of given node in binary tree
+// iterative approach
+/*
+binary tree
+root is at level 1
+
+                20
+              /   \
+            10    30
+           / \    / \
+          5  15  25  40
+             /
+            12
+*/
+#include <iostream>
+#include <queue>
+using namespace std;
+
+// node of binary tree
+struct node{
+	int data;
+	node *left;
+	node *right;
+};
+
+// utility function to create
+// a new node
+node * getnode(int data){
+	node * newnode = new node();
+	newnode->data = data;
+	newnode->left = NULL;
+	newnode->right = NULL;
+}
+
+// utility function to return level of given node
+int printkdistant(node *root, int klevel){
+	queue<node*> q;
+	int level = 1;
+	q.push(root);
+	// extra NULL is pushed to keep track
+	// of all the nodes to be pushed before
+	// level is incremented by 1
+	q.push(NULL);
+	while(!q.empty()){
+		node *temp = q.front();
+		if(level == klevel && temp != NULL){
+			cout << temp->data << " "; 
+		}
+		q.pop();
+		if(temp == NULL){
+			if(q.front())q.push(NULL);
+			level +=1;
+			// break the loop if level exceeds
+			// level given upto which program 
+			// has to traverse
+			if(level > klevel){
+				break;
+			}
+		}
+		else{
+			if(temp->left){
+				q.push(temp->left);
+			}
+			if(temp->right){
+				q.push(temp->right);
+			}
+		}
+	}
+	cout << endl;
+	if(klevel > level)
+		return 0;
+	else return 1;
+}
+int main(){
+	// create a binary tree
+	node *root = getnode(20);
+	root->left = getnode(10);
+	root->right = getnode(30);
+	root->left->left = getnode(5);
+	root->left->right = getnode(15);
+	root->left->right->left = getnode(12);
+	root->right->left = getnode(25);
+	root->right->right = getnode(40);
+
+	// return level of node
+	cout << "data at level 1 : ";
+	int ret = printkdistant(root, 1);
+	if(ret == 0) cout << "levels in tree are less than number you given" << endl;
+	cout << "data at level 2 : ";
+	ret = printkdistant(root, 2);
+	if(ret == 0) cout << "levels in tree are less than number you given" << endl;
+	cout << "data at level 3 : ";
+	ret = printkdistant(root, 3);
+	if(ret == 0) cout << "levels in tree are less than number you given" << endl;
+	cout << "data at level 6 : ";
+	ret = printkdistant(root,6);
+	if(ret == 0) cout << "levels in tree are less than number you given" << endl;
+	return 0;
+}
